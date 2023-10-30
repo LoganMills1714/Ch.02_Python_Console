a57fb13957d21351003b0ad3f76b9152c710491a
Logan Mills <millslogan1714@gmail.com>
2023-10-30 09:42:40 -0500

    Filled in answers

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -1,25 +1,26 @@
 '''
-2.0 Jedi Training (20pts)  Name:________________
+2.0 Jedi Training (20pts)  Name:Logan Mills
 
 UPLOAD YOUR CH 2 PDF LOG! (6pts)
 
 1.) How do you enter a single line comment in a program? Give an example. (1pt)
-
+You use the # symbol to begin a single line comment
+# This is a single line comment
 
 2.) If a=2 and b=5, predict all of the following below and record your predictions. (9pts)
 Then in the Python Console window, enter a=2 and b=5 and then all of the following and record the actual output.
 If the output is an error record the error and try to determine what the error is!
 Comment about any of your predictions that didn't match the actual output
 
-b/a                 =
-b//a                =
-b**a                =
-b%a                 =
-a+B                 =
-type(42)            =
-type(42.0)          =
-type("C3PO")        =
-type(True)          =
+b/a                 = 2.5 Correct
+b//a                = 2 Correct
+b**a                = 25 Correct
+b%a                 = idk Wrong, 1
+a+B                 = Error, capitalization (you defined b not B) Correct
+type(42)            = Integer Correct
+type(42.0)          = Float Correct
+type("C3PO")        = String Correct
+type(True)          = Boolean Correct
 
 
 3.) Predict what would be the final output of (a) and type(a) if you enter the following 5 lines        (2pts)
@@ -31,22 +32,23 @@ a*=10
 a/=2
 a+=12
 a-=7
-a             =
-type(a)       =
+a             = 15, 15.0
+type(a)       = Integer wrong, float
 
 
 4.) What is the mistake in the following code? Comment it and fix it!  (1pt)
 
 x,y = (4,5)
-a = 3(x + y)
-a
-
+a = x + y  # Syntax, cant use parenthesis like that
+a *= 3
+print(a)
 
 5.) What is the mistake in the following code so it will calculate the average? Comment it and fix it!  (1pt)
 
 x,y,z =(3,4,5)
-ave = x+y+z/3
-ave
-
+ave = x+y+z  # Order of operations, doing division first instead of last
+ave /= 3  
+print(ave)  # Wasn't able to print
 
 '''
+
